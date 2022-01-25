575b6396f1bc680aa1aca9d2dc5bcb49828aea94
Taranpreet Singh <taransingh26311@gmail.com>
2022-01-25 21:15:04 +0530

    Added fix

--- export.sh
+++ export.sh
@@ -1,2 +1,2 @@
-export CURRENT_VERSION="$(grep  "\release: " charts-test/values.yaml | awk -F ": " '{print $2}')"
-export CHART_VERSION="$(grep  "\version: " charts-test/Chart.yaml | awk -F ": " '{print $2}')"
+export CURRENT_VERSION="$(grep  "\release: " charts-test/values.yaml | sed 's/[^,:]*://g')"
+export CHART_VERSION="$(grep  "\version: " charts-test/Chart.yaml | sed 's/[^,:]*://g')"
